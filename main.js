54a7df9b77d28898782df804f7b776f71347053d
unnihc <unnihc369@gmail.com>
2024-02-22 00:02:08 +0530

    add about section and change navbar scrolling

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    const scrollPosition = window.scrollY;
+// window.addEventListener('scroll', () => {
+//     const nav = document.querySelector('nav');
+//     const scrollPosition = window.scrollY;
   
-    if (scrollPosition > 100) { /* Adjust scroll threshold as needed */
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
-    }
+//     if (scrollPosition > 100) { /* Adjust scroll threshold as needed */
+//       nav.classList.add('scrolled');
+//     } else {
+//       nav.classList.remove('scrolled');
+//     }
+//   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the home button element
+  var homeBtn = document.getElementById('home-btn');
+
+  // Add click event listener to the home button
+  homeBtn.addEventListener('click', function (event) {
+    // Prevent default anchor behavior
+    event.preventDefault();
+
+    // Scroll to the top of the page smoothly
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   });
+});
+
+
